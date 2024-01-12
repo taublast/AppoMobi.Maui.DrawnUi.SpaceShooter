305ffcb0040f5212948a6436b6d6666e94438cf1
Nick Kovalsky <nickyboom@mail.ru>
2024-01-12 20:43:54 +0300

    Windows Keys

--- src/Platforms/MacCatalyst/AppDelegate.cs
+++ src/Platforms/MacCatalyst/AppDelegate.cs
@@ -7,26 +7,33 @@ namespace SpaceShooter
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate
     {
+
         public override void PressesBegan(NSSet<UIPress> presses, UIPressesEvent evt)
         {
-            //base.PressesBegan(presses, evt);
-
+            bool consumed = false;
             foreach (UIPress press in presses)
             {
                 if (press.Type == (UIPressType)2044) //SPACE
                 {
+                    consumed = true;
                     MauiGame.KeyboardPressed(GameKey.Space);
                 }
                 else
                 if (press.Type == (UIPressType)2080) //LEFT
                 {
+                    consumed = true;
                     MauiGame.KeyboardPressed(GameKey.Left);
                 }
                 else
                 if (press.Type == (UIPressType)2079) //RIGHT
                 {
+                    consumed = true;
                     MauiGame.KeyboardPressed(GameKey.Right);
                 }
+            }
+            if (!consumed)
+            {
+                base.PressesBegan(presses, evt);
             }
         }
 
--- src/Platforms/Windows/App.xaml.cs
+++ src/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
+using SpaceShooter.Game;
+using Windows.System;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,5 +22,51 @@ namespace SpaceShooter.WinUI
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(args);
+
+            var appWindow = this.Application.Windows.First() as Microsoft.Maui.Controls.Window;
+            var window = appWindow.Handler.PlatformView as Microsoft.Maui.MauiWinUIWindow;
+
+            //hook keys
+            var native = window.Content as Microsoft.UI.Xaml.Controls.Panel;
+            native.PreviewKeyUp += (sender, args) =>
+            {
+                if (args.Key == VirtualKey.Space)
+                {
+                    MauiGame.KeyboardReleased(GameKey.Space);
+                }
+                else
+                if (args.Key == VirtualKey.Left)
+                {
+                    MauiGame.KeyboardReleased(GameKey.Left);
+                }
+                else
+                if (args.Key == VirtualKey.Right)
+                {
+                    MauiGame.KeyboardReleased(GameKey.Right);
+                }
+            };
+            native.PreviewKeyDown += (sender, args) =>
+            {
+                if (args.Key == VirtualKey.Space)
+                {
+                    MauiGame.KeyboardPressed(GameKey.Space);
+                }
+                else
+                if (args.Key == VirtualKey.Left)
+                {
+                    MauiGame.KeyboardPressed(GameKey.Left);
+                }
+                else
+                if (args.Key == VirtualKey.Right)
+                {
+                    MauiGame.KeyboardPressed(GameKey.Right);
+                }
+
+            };
+        }
     }
 }
